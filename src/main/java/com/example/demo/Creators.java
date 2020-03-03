2b461cbcf5ae4b019a4cb9f071b78a85222af1a1
Kate <katekarpenko75@gmail.com>
2020-03-03 20:58:53 +0200

    Change name Creators

--- src/main/java/com/example/demo/Creators.java
+++ src/main/java/com/example/demo/Creators.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 public class Creators {
-    private final String nameOfCreators="SEREGA";
+    private final String nameOfCreators="SEREGA-VODOLAZ";
     private final int costOfCreators=3;
 
     public int getCostOfCreators() {
